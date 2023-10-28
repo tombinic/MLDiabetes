659991faf413d11b15f44c7f347ff21d2094ecd7
Nicolò Tombini <91635053+tombinic@users.noreply.github.com>
2023-10-28 15:47:50 +0200

    Update modelComparison.py

--- modelComparison.py
+++ modelComparison.py
@@ -1,23 +1,14 @@
-def models_comparison(X_train, y_train):
-    print("\nSearching best algorithm\n")
+def fine_tuning_gb_gs(X_train, y_train):
+    print("\nGrid Search\n")
     kf = StratifiedKFold(n_splits = constants.N_SPLITS, random_state = None, shuffle = False)
-    logistic_regression = LogisticRegression(solver = "liblinear")
-    random_forest = RandomForestClassifier()
-    ada_boost = AdaBoostClassifier()
-    gradient_boosting = GradientBoostingClassifier()
-    decision_tree = DecisionTreeClassifier()
-
-    acc_regr, acc_rf, acc_ab, acc_gb, acc_dt = 0, 0, 0, 0, 0
-
-    for train_index, validation_index in kf.split(X_train, y_train):
-        xt = X_train.iloc[train_index]
-        xv = X_train.iloc[validation_index]
-        yt = y_train.iloc[train_index]
-        yv = y_train.iloc[validation_index]
-
-        logistic_regression.fit(xt, yt)
-        random_forest.fit(xt, yt)
-        logistic_regression.fit(xt, yt)
-        ada_boost.fit(xt, yt)
-        gradient_boosting.fit(xt, yt)
-        decision_tree.fit(xt, yt)
+    for i in range(constants.N_DEFAULT_TREES - 25, constants.N_DEFAULT_TREES + 100, 25):
+        for j in range(constants.MAX_DEFAULT_DEPTH, constants.MAX_DEFAULT_DEPTH + 5, 1):
+            for h in constants.N_LEARNING_RATE:
+                final_gradient_boosting = GradientBoostingClassifier(n_estimators = i, learning_rate = j, max_depth = h)
+                for train_index, validation_index in kf.split(X_train, y_train):
+                    xt = X_train.iloc[train_index]
+                    xv = X_train.iloc[validation_index]
+                    yt = y_train.iloc[train_index]
+                    yv = y_train.iloc[validation_index]
+                    final_gradient_boosting.fit(xt, yt)
+                    y_pred_vt_gb = final_gradient_boosting.predict(xv)
